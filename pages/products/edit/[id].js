58c633d5eed01ec82fdfdf6eb2a120351c5ab1b2
Ziad Gaafar <ziadgaafar@protonmail.com>
2021-09-01 09:29:13 +0200

    added top margin

--- pages/products/edit/[id].js
+++ pages/products/edit/[id].js
@@ -169,221 +169,214 @@ const EditProduct = ({ product }) => {
         <title>Edit Product</title>
       </Head>
       {auth?.user?.role === "admin" && (
-        <Box marginTop={2}>
-          <form onSubmit={formik.handleSubmit}>
-            <Grid container>
-              <Grid item xs={12} md={6}>
-                <Box padding={`0 ${mdUp ? "16px 0 0" : "16px"}`}>
-                  <Grid container direction="column">
-                    <Grid item style={{ marginBottom: 16 }}>
-                      <TextField
-                        name="title"
-                        label="Title"
-                        variant="outlined"
-                        value={formik.values.title}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.title && Boolean(formik.errors.title)
-                        }
-                        helperText={formik.touched.title && formik.errors.title}
-                        fullWidth
-                      />
-                    </Grid>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <Box padding={`0 ${mdUp ? "16px 0 0" : "16px"}`} marginTop={1}>
+                <Grid container direction="column">
+                  <Grid item style={{ marginBottom: 16 }}>
+                    <TextField
+                      name="title"
+                      label="Title"
+                      variant="outlined"
+                      value={formik.values.title}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.title && Boolean(formik.errors.title)
+                      }
+                      helperText={formik.touched.title && formik.errors.title}
+                      fullWidth
+                    />
+                  </Grid>
 
-                    <Grid item style={{ marginBottom: 16 }}>
-                      <Grid container>
-                        <Grid item xs={6}>
-                          <TextField
-                            name="price"
-                            label="Price"
-                            type="number"
-                            variant="outlined"
-                            InputProps={{ inputProps: { min: 0 } }}
-                            value={formik.values.price}
-                            onChange={formik.handleChange}
-                            error={
-                              formik.touched.price &&
-                              Boolean(formik.errors.price)
-                            }
-                            helperText={
-                              formik.touched.price && formik.errors.price
-                            }
-                            fullWidth
-                          />
-                        </Grid>
-                        <Grid item xs={6}>
-                          <TextField
-                            name="inStock"
-                            label="In Stock"
-                            type="number"
-                            variant="outlined"
-                            InputProps={{ inputProps: { min: 0 } }}
-                            value={formik.values.inStock}
-                            onChange={formik.handleChange}
-                            error={
-                              formik.touched.inStock &&
-                              Boolean(formik.errors.inStock)
-                            }
-                            helperText={
-                              formik.touched.inStock && formik.errors.inStock
-                            }
-                            fullWidth
-                          />
-                        </Grid>
+                  <Grid item style={{ marginBottom: 16 }}>
+                    <Grid container>
+                      <Grid item xs={6}>
+                        <TextField
+                          name="price"
+                          label="Price"
+                          type="number"
+                          variant="outlined"
+                          InputProps={{ inputProps: { min: 0 } }}
+                          value={formik.values.price}
+                          onChange={formik.handleChange}
+                          error={
+                            formik.touched.price && Boolean(formik.errors.price)
+                          }
+                          helperText={
+                            formik.touched.price && formik.errors.price
+                          }
+                          fullWidth
+                        />
                       </Grid>
-                    </Grid>
-
-                    <Grid item style={{ marginBottom: 16 }}>
-                      <TextField
-                        name="content"
-                        multiline
-                        minRows={4}
-                        maxRows={4}
-                        label="Content"
-                        variant="outlined"
-                        value={formik.values.content}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.content &&
-                          Boolean(formik.errors.content)
-                        }
-                        helperText={
-                          formik.touched.content && formik.errors.content
-                        }
-                        fullWidth
-                      />
-                    </Grid>
-
-                    <Grid item style={{ marginBottom: 16 }}>
-                      <TextField
-                        name="description"
-                        multiline
-                        minRows={6}
-                        maxRows={6}
-                        label="Description"
-                        variant="outlined"
-                        value={formik.values.description}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.description &&
-                          Boolean(formik.errors.description)
-                        }
-                        helperText={
-                          formik.touched.description &&
-                          formik.errors.description
-                        }
-                        fullWidth
-                      />
-                    </Grid>
-
-                    <Grid item>
-                      <TextField
-                        name="category"
-                        select
-                        label="Category"
-                        variant="outlined"
-                        color="primary"
-                        value={formik.values.category}
-                        onChange={formik.handleChange}
-                        error={
-                          formik.touched.category &&
-                          Boolean(formik.errors.category)
-                        }
-                        helperText={
-                          formik.touched.category && formik.errors.category
-                        }
-                        fullWidth
-                      >
-                        {categories &&
-                          categories.map((c) => (
-                            <MenuItem key={c._id} value={c._id}>
-                              {c.name}
-                            </MenuItem>
-                          ))}
-                      </TextField>
+                      <Grid item xs={6}>
+                        <TextField
+                          name="inStock"
+                          label="In Stock"
+                          type="number"
+                          variant="outlined"
+                          InputProps={{ inputProps: { min: 0 } }}
+                          value={formik.values.inStock}
+                          onChange={formik.handleChange}
+                          error={
+                            formik.touched.inStock &&
+                            Boolean(formik.errors.inStock)
+                          }
+                          helperText={
+                            formik.touched.inStock && formik.errors.inStock
+                          }
+                          fullWidth
+                        />
+                      </Grid>
                     </Grid>
                   </Grid>
-                </Box>
-              </Grid>
 
-              <Grid item xs={12} md={6}>
-                <Box padding={mdUp ? "0" : "0 16px"}>
-                  <Box marginTop={mdUp ? 0 : 2}>
-                    <Button
+                  <Grid item style={{ marginBottom: 16 }}>
+                    <TextField
+                      name="content"
+                      multiline
+                      minRows={4}
+                      maxRows={4}
+                      label="Content"
+                      variant="outlined"
+                      value={formik.values.content}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.content && Boolean(formik.errors.content)
+                      }
+                      helperText={
+                        formik.touched.content && formik.errors.content
+                      }
                       fullWidth
-                      component="label"
-                      variant="contained"
-                      color="secondary"
+                    />
+                  </Grid>
+
+                  <Grid item style={{ marginBottom: 16 }}>
+                    <TextField
+                      name="description"
+                      multiline
+                      minRows={6}
+                      maxRows={6}
+                      label="Description"
+                      variant="outlined"
+                      value={formik.values.description}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.description &&
+                        Boolean(formik.errors.description)
+                      }
+                      helperText={
+                        formik.touched.description && formik.errors.description
+                      }
+                      fullWidth
+                    />
+                  </Grid>
+
+                  <Grid item>
+                    <TextField
+                      name="category"
+                      select
+                      label="Category"
+                      variant="outlined"
+                      color="primary"
+                      value={formik.values.category}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.category &&
+                        Boolean(formik.errors.category)
+                      }
+                      helperText={
+                        formik.touched.category && formik.errors.category
+                      }
+                      fullWidth
                     >
-                      Upload Images
-                      <input
-                        type="file"
-                        onChange={handleImagesUpload}
-                        hidden
-                        multiple
-                        accept="image/*"
-                      />
-                    </Button>
-                    <Box marginTop={2}>
-                      <Grid container>
-                        {imagesArr.length > 0 &&
-                          imagesArr.map((image, index) => (
-                            <Grid
-                              item
-                              xs={index === 0 ? 12 : 3}
-                              key={
-                                image.public_id ? image.public_id : image.name
+                      {categories &&
+                        categories.map((c) => (
+                          <MenuItem key={c._id} value={c._id}>
+                            {c.name}
+                          </MenuItem>
+                        ))}
+                    </TextField>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Box padding={mdUp ? "0" : "0 16px"} marginTop={1}>
+                <Box marginTop={mdUp ? 0 : 2}>
+                  <Button
+                    fullWidth
+                    component="label"
+                    variant="contained"
+                    color="secondary"
+                  >
+                    Upload Images
+                    <input
+                      type="file"
+                      onChange={handleImagesUpload}
+                      hidden
+                      multiple
+                      accept="image/*"
+                    />
+                  </Button>
+                  <Box marginTop={2}>
+                    <Grid container>
+                      {imagesArr.length > 0 &&
+                        imagesArr.map((image, index) => (
+                          <Grid
+                            item
+                            xs={index === 0 ? 12 : 3}
+                            key={image.public_id ? image.public_id : image.name}
+                            style={{ position: "relative" }}
+                          >
+                            <img
+                              src={
+                                image.url
+                                  ? image.url
+                                  : URL.createObjectURL(image)
                               }
-                              style={{ position: "relative" }}
+                              alt={image.name}
+                              style={{
+                                width: "100%",
+                                height: index === 0 ? 350 : 80,
+                                objectFit: "cover",
+                                objectPosition: "center",
+                                border: "1px solid grey",
+                                padding: 2,
+                                borderRadius: 6,
+                              }}
+                            />
+                            <IconButton
+                              size="small"
+                              onClick={() => handleRemoveImage(index)}
+                              style={{
+                                position: "absolute",
+                                top: 0,
+                                right: 0,
+                                zIndex: 2,
+                                background: "white",
+                              }}
                             >
-                              <img
-                                src={
-                                  image.url
-                                    ? image.url
-                                    : URL.createObjectURL(image)
-                                }
-                                alt={image.name}
-                                style={{
-                                  width: "100%",
-                                  height: index === 0 ? 350 : 80,
-                                  objectFit: "cover",
-                                  objectPosition: "center",
-                                  border: "1px solid grey",
-                                  padding: 2,
-                                  borderRadius: 6,
-                                }}
-                              />
-                              <IconButton
-                                size="small"
-                                onClick={() => handleRemoveImage(index)}
-                                style={{
-                                  position: "absolute",
-                                  top: 0,
-                                  right: 0,
-                                  zIndex: 2,
-                                  background: "white",
-                                }}
-                              >
-                                <Close fontSize="small" color="secondary" />
-                              </IconButton>
-                            </Grid>
-                          ))}
-                      </Grid>
-                    </Box>
+                              <Close fontSize="small" color="secondary" />
+                            </IconButton>
+                          </Grid>
+                        ))}
+                    </Grid>
                   </Box>
                 </Box>
-              </Grid>
-
-              <Grid container justifyContent="center">
-                <Box marginTop={2}>
-                  <Button type="submit" variant="contained" color="primary">
-                    edit
-                  </Button>
-                </Box>
-              </Grid>
+              </Box>
             </Grid>
-          </form>
-        </Box>
+
+            <Grid container justifyContent="center">
+              <Box marginTop={2}>
+                <Button type="submit" variant="contained" color="primary">
+                  edit
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </form>
       )}
     </>
   );
